0942611e8e744838d9ddbfa34d7e63466bf54aa2
JonatanValdivia <jonatan.viniciusvaldivia@outlook.com>
2021-08-09 15:50:36 -0300

    Variaveis de ambiente adicionadas

--- App/Core/Model.php
+++ App/Core/Model.php
@@ -6,8 +6,12 @@ class Model{
   private static $conexao;
 
   public static function conexaoDB(){
+    $host = $_ENV["database_host"];
+    $user = $_ENV["database_user"];
+    $pass = $_ENV["database_pass"];
+
     if(!isset(self::$conexao)){
-      self::$conexao = new \PDO("mysql:host=localhost;post=3306;dbname=FastParking;", "root", "bcd127"); 
+      self::$conexao = new \PDO("mysql:host=$host;post=3306;dbname=fastparking;", $user, $pass); 
     }
 
     return self::$conexao;
